02ee88b5dd591f40686526ad59d96d58da461a81
BattsEA <b-eugen@yandex.ru>
2017-09-08 17:36:27 +0300

    Add apps

---
+++ sovisu/static/js/triangle.js
@@ -0,0 +1,361 @@
+﻿
+
+function drawIndicator(obj, url, typep, eu) {
+    $.ajax(
+        {
+            url: url,
+            data: {
+                id: obj
+            },
+            type: 'POST',
+            beforeSend: function () {
+            },
+            success: function (data) {
+                drawGaude(data);
+                drawArc(data.max, data.min, 0, data.id, typep, eu);
+            },
+            error: function () {
+
+            }
+        });
+}
+
+function drawChart(obj, url, bind) {
+    $.ajax(
+        {
+            url: url,
+            data: {
+                id: obj
+            },
+            timeout: 600000,
+            type: 'POST',
+            beforeSend: function () {
+            },
+            success: function (data) {
+                var w = $("#cc_" + data.id).innerWidth() - 15;
+                var h = $("#cc_" + data.id).innerHeight() - 5;
+                $("#cht_" + data.id).width(w).height(h);
+                $.plot("#cht_" + data.id, data.FlotData, data.FlotOpt);
+                $(".legend>table").css({ top: 16 });
+                if (!bind) {
+                    $("#cht_" + data.id).bind("plothover", function (event, pos, item) {
+                        if (item) {
+                            var x = item.datapoint[0],
+                                y = item.datapoint[1].toFixed(1);
+
+                            $("#tooltip").html(item.series.label + ":" + formatTime(x) + " = " + y)
+                                .css({ top: item.pageY + 5, left: item.pageX - 150 })
+                                .fadeIn(200);
+                        } else {
+                            $("#tooltip").hide();
+                        }
+                    });
+                    bind = true;
+                }
+                window.setTimeout(function () {
+                    drawChart(obj, url, bind);
+                }, 600000);
+            },
+            error: function() {
+                window.setTimeout(function () {
+                    drawChart(obj, url, bind);
+                }, 600000);
+            }
+        });
+}
+
+function drawtriangle(data) {
+    var cnv = $("#triangle");
+    var w = $("#dv_triangle").innerWidth();
+    var h = $("#dv_triangle").innerHeight();
+    if (h > w) {
+        h = w;
+    } else {
+        w = h;
+    }
+    var x = w *.5;
+    var y = h *.5;
+    var l = x*0.9
+    $("#dv_triangle").width(w).height(h)
+    cnv.width(w).height(h);
+    cnv.attr('width', w).attr('height', h);
+    var context = cnv.get(0).getContext('2d');
+    var angle1 = 270 * (Math.PI / 180);
+    var angle2 = 150 * (Math.PI / 180);
+    var angle3 = 30 * (Math.PI / 180);
+
+    var k = 0.3;
+    var ks1 = 2.3;
+    var ks2 = 1.6;
+    context.beginPath();
+    context.moveTo(3*k*l*Math.cos(angle1) + x, 3*k*l*Math.sin(angle1) + y);
+    context.lineTo(3*k*l*Math.cos(angle2) + x, 3*k*l*Math.sin(angle2) + y);
+    context.lineTo(3*k*l*Math.cos(angle3) + x, 3*k*l*Math.sin(angle3) + y);
+    context.fillStyle = 'rgba(255, 17, 31, 0.15)';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'rgba(255, 17, 31, 0.15)';
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo(ks1*k*l*Math.cos(angle1) + x, ks1*k*l*Math.sin(angle1) + y);
+    context.lineTo(ks1*k*l*Math.cos(angle2) + x, ks1*k*l*Math.sin(angle2) + y);
+    context.lineTo(ks1*k*l*Math.cos(angle3) + x, ks1*k*l*Math.sin(angle3) + y);
+    context.fillStyle = 'rgba(255, 255, 255, 1.0)';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'rgba(255, 255, 255, 1.0)';
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo(ks1*k*l*Math.cos(angle1) + x, ks1*k*l*Math.sin(angle1) + y);
+    context.lineTo(ks1*k*l*Math.cos(angle2) + x, ks1*k*l*Math.sin(angle2) + y);
+    context.lineTo(ks1*k*l*Math.cos(angle3) + x, ks1*k*l*Math.sin(angle3) + y);
+    context.fillStyle='#ffffff'
+    context.fillStyle = 'rgba(255, 110, 0, 0.1)';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'rgba(255, 110, 0, 0.1)';
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo(ks2*k*l*Math.cos(angle1) + x, ks2*k*l*Math.sin(angle1) + y);
+    context.lineTo(ks2*k*l*Math.cos(angle2) + x, ks2*k*l*Math.sin(angle2) + y);
+    context.lineTo(ks2*k*l*Math.cos(angle3) + x, ks2*k*l*Math.sin(angle3) + y);
+    context.fillStyle = 'rgba(255, 255, 255, 1.0)';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'rgba(255, 255, 255, 1.0)';
+    context.stroke();
+    context.closePath();
+
+    context.beginPath();
+    context.moveTo(ks2*k*l*Math.cos(angle1) + x, ks2*k*l*Math.sin(angle1) + y);
+    context.lineTo(ks2*k*l*Math.cos(angle2) + x, ks2*k*l*Math.sin(angle2) + y);
+    context.lineTo(ks2*k*l*Math.cos(angle3) + x, ks2*k*l*Math.sin(angle3) + y);
+    context.fillStyle = 'rgba(26, 255, 7, 0.1)';
+    context.fill();
+    context.lineWidth = 1;
+    context.strokeStyle = 'rgba(26, 255, 7, 0.1)';
+    context.stroke();
+    context.closePath();
+
+
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(l*Math.cos(angle1) + x, l*Math.sin(angle1) + y)
+    context.lineWidth = 3;
+    context.fillStyle = '#525457';
+    context.strokeStyle = '#525457';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(l*Math.cos(angle2) + x, l*Math.sin(angle2) + y)
+    context.lineWidth = 3;
+    context.fillStyle = '#525457';
+    context.strokeStyle = '#525457';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.moveTo(x, y);
+    context.lineTo(l*Math.cos(angle3) + x, l*Math.sin(angle3) + y)
+    context.lineWidth = 3;
+    context.fillStyle = '#525457';
+    context.strokeStyle = '#525457';
+    context.stroke();
+    context.closePath();
+
+    cnv.show()
+/*    var min = data.min;
+    var max = data.max;
+    var arcForData = 1.5 * Math.PI / (max - min);
+    var offset = 0.75 * Math.PI - arcForData * min;
+    rs = rs - rs * 0.1;
+    context.beginPath();
+    context.arc(x, y, rs, arcForData * min + offset, arcForData * max + offset);
+    context.lineWidth = 4;
+    context.strokeStyle = '#e80c4d';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.arc(x, y, rs, arcForData * data.lolo + offset, arcForData * data.hihi + offset);
+    context.lineWidth = 4;
+    context.strokeStyle = '#E6D844';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.arc(x, y, rs, arcForData * data.lohi + offset, arcForData * data.hilo + offset);
+    context.lineWidth = 4;
+    context.strokeStyle = '#00ff00';
+    context.stroke();
+    context.closePath();
+    var rs1 = rs - rs * 0.1;
+    rs = rs + rs * .01;
+    context.beginPath();
+    context.moveTo(rs1 * Math.cos(arcForData * min + offset) + x, rs1 * Math.sin(arcForData * min + offset) + y);
+    context.lineTo(rs * Math.cos(arcForData * min + offset) + x, rs * Math.sin(arcForData * min + offset) + y);
+    context.lineWidth = 4;
+    context.strokeStyle = '#e80c4d';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.moveTo(rs1 * Math.cos(arcForData * data.lolo + offset) + x, rs1 * Math.sin(arcForData * data.lolo + offset) + y);
+    context.lineTo(rs * Math.cos(arcForData * data.lolo + offset) + x, rs * Math.sin(arcForData * data.lolo + offset) + y);
+    context.lineWidth = 4;
+    context.strokeStyle = '#E6D844';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.moveTo(rs1 * Math.cos(arcForData * data.lohi + offset) + x, rs1 * Math.sin(arcForData * data.lohi + offset) + y);
+    context.lineTo(rs * Math.cos(arcForData * data.lohi + offset) + x, rs * Math.sin(arcForData * data.lohi + offset) + y);
+    context.lineWidth = 4;
+    context.strokeStyle = '#00ff00';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.moveTo(rs1 * Math.cos(arcForData * data.hilo + offset) + x, rs1 * Math.sin(arcForData * data.hilo + offset) + y);
+    context.lineTo(rs * Math.cos(arcForData * data.hilo + offset) + x, rs * Math.sin(arcForData * data.hilo + offset) + y);
+    context.lineWidth = 4;
+    context.strokeStyle = '#00ff00';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.moveTo(rs1 * Math.cos(arcForData * data.hihi + offset) + x, rs1 * Math.sin(arcForData * data.hihi + offset) + y);
+    context.lineTo(rs * Math.cos(arcForData * data.hihi + offset) + x, rs * Math.sin(arcForData * data.hihi + offset) + y);
+    context.lineWidth = 4;
+    context.strokeStyle = '#E6D844';
+    context.stroke();
+    context.closePath();
+    context.beginPath();
+    context.moveTo(rs1 * Math.cos(arcForData * max + offset) + x, rs1 * Math.sin(arcForData * max + offset) + y);
+    context.lineTo(rs * Math.cos(arcForData * max + offset) + x, rs * Math.sin(arcForData * max + offset) + y);
+    context.lineWidth = 4;
+    context.strokeStyle = '#e80c4d';
+    context.stroke();
+    var point = w * 0.05;
+    rs1 = rs1 - rs1 * 0.15;
+    context.fillStyle = "#F2F2F2";
+    context.font = point + "pt Verdana";
+    //   x = x - x * 0.05 * (360 - arcForData);
+    var ktoeff = 0.5
+    context.fillText(min, rs1 * Math.cos(arcForData * min + offset) + x - ktoeff * (arcForData * min), rs1 * Math.sin(arcForData * min + offset) + y + 15);
+    context.fillText(data.lolo, rs1 * Math.cos(arcForData * data.lolo + offset) + x - ktoeff * (arcForData * data.lolo), rs1 * Math.sin(arcForData * data.lolo + offset) + y);
+    context.fillText(data.lohi, rs1 * Math.cos(arcForData * data.lohi + offset) + x - ktoeff * (arcForData * data.lohi), rs1 * Math.sin(arcForData * data.lohi + offset) + y);
+    rs1 = rs1 - point * 0.75;
+    context.fillText(data.hilo, rs1 * Math.cos(arcForData * data.hilo + offset) + x - ktoeff * (arcForData * data.hilo), rs1 * Math.sin(arcForData * data.hilo + offset) + y);
+    context.fillText(data.hihi, rs1 * Math.cos(arcForData * data.hihi + offset) + x - ktoeff * (arcForData * data.hihi), rs1 * Math.sin(arcForData * data.hihi + offset) + y);
+    context.fillText(max, rs1 * Math.cos(arcForData * max + offset) + x - ktoeff * (arcForData * max), rs1 * Math.sin(arcForData * max + offset) + y);
+*/
+}
+
+function drawArc(max, min, val, obj, typep, eu) {
+    var cnv = $("#cnv_" + obj);
+    var w = cnv.width();
+    var h = cnv.height();
+    var wind = w * 0.55;
+    var hind = h * 0.20;
+    var position = cnv.position();
+    var ind = $("#ind_g_" + obj);
+    ind.width(wind).height(hind);
+    ind.attr('width', wind).attr('height', hind);
+    ind.text(typep + " = ???" + eu);
+    ind.css("top", position.top + h - hind);
+    ind.css("left", position.left + w * 0.5 - wind * 0.5);
+    ind.show();
+    cnv = $("#ar_" + obj);
+    cnv.width(w).height(h);
+    cnv.attr('width', w).attr('height', h);
+    cnv.offset({ top: position.top, left: position.left });
+    var arcForData = 1.5 * Math.PI / (max - min);
+    var offset = 1.75 * Math.PI - arcForData * min;
+    var rs = 0;
+    if (w > h) {
+        rs = h / 2;
+    } else {
+        rs = w / 2;
+    }
+    rs -= 6;
+    rs = rs - rs * 0.1;
+    var offsetArc = rs * 0.2;
+    var context = cnv.get(0).getContext('2d');
+    context.clearRect(0, 0, w, h);
+    context.translate(w / 2, h / 2);
+    context.rotate(val * arcForData + offset);
+    context.beginPath();
+    context.moveTo(-rs, 0);
+    context.lineTo(offsetArc, h * 0.045);
+    context.lineTo(0, 0);
+    context.lineTo(offsetArc, -h * 0.045);
+    context.lineTo(-rs, 0);
+    context.fillStyle = '#F2F2F2';
+    context.fill();
+    context.lineWidth = 2;
+    context.strokeStyle = '#d2691e';
+    context.stroke();
+    context.closePath();
+    cnv.show();
+}
+
+
+
+function getIndicator(obj, url, round, typep, eu, updatagaude) {
+    $.ajax(
+        {
+            url: url,
+            data: {
+                id: obj
+            },
+            timeout: 10000,
+            type: 'POST',
+            beforeSend: function () {
+            },
+            success: function (data) {
+                var val = data.val;
+                if (val > data.room.max)
+                    val = data.room.max;
+                if (val < data.room.min)
+                    val = data.room.min;
+                if (updatagaude != null)
+                    drawGaude(data.room);
+                drawArc(data.room.max, data.room.min, val, data.id);
+                $("#ind_g_" + data.id).text(typep + " = " + (data.val).toFixed(round) + eu);
+                $("#rh_" + data.id).css("background-color", "#686868");
+                if(val > data.room.hilo || val < data.room.lohi)
+                    $("#rh_" + data.id).css("background-color", "#E6D844");
+                if(val > data.room.hihi || val < data.room.lolo)
+                    $("#rh_" + data.id).css("background-color", "#e80c4d");
+                $("#counter-digest > div").each(function (index) {
+                    if(index < data.counter.length)
+                      $("#n" + (index+1)).text(data.counter.substr(index, 1));
+                });
+                window.setTimeout(function () {
+                    getIndicator(obj, url, round, typep, eu, updatagaude);
+                }, 5000);
+            },
+            error: function () {
+                window.setTimeout(function () {
+                    getIndicator(obj, url, round, typep, eu, updatagaude);
+                }, 5000);
+            }
+        });
+}
+
+function formatTime(mseconds) {
+    var time = new Date(mseconds);
+    var mon = time.getMonth() + 1;
+    var minut = time.getMinutes();
+    var second = time.getSeconds();
+    if (mon <= 9) {
+        mon = "0" + mon;
+    }
+    if (minut <= 9)
+        minut = "0" + minut;
+    if (second <= 9)
+        second = "0" + second;
+    var clockf = " " + time.getUTCDate() + "." + mon + "." + time.getFullYear() + " " + time.getUTCHours() + ":" + minut;
+    return clockf;
+}
